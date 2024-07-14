00f61ec61f3f5b44d53f0951fcca8ff3fe50b4b1
ayoubioussoufa <ayoubioussoufa@gmail.com>
2024-07-14 13:37:47 +0100

    DataBase added but still need some few modifications

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require("uuid");
+let { Wallet } = require('./database');
 var { Web3 } = require("web3");
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -13,6 +15,10 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+// Generate a UUID v4
+const uuid = uuidv4();
+console.log(`UUID: ${uuid}`);
 
 var provider = "https://sepolia.infura.io/v3/6483579a38ee4626b9a67d15ca7fef2d";
 // var web3Provider = new Web3(provider);
@@ -33,23 +39,18 @@ app.get('/api/balance/:address', async (req, res) => {
 });
 
 app.post('/create-wallet', async(req, res) => {
+    const userId = req.body.userId;
+    console.log(userId);
     if (req.body.action === 'create_wallet') {
         const account = web3.eth.accounts.create();
-        // walletData = {
-            //     address: account.address,
-            //     privateKey: account.privateKey
-            // };
+            storeEncryptedPrivateKey(userId, account.privateKey);
             const privateKeyBuffer = Buffer.from(account.privateKey.slice(2), 'hex');
             const publicKey = ethUtil.privateToPublic(privateKeyBuffer).toString('hex');
-            // privateKey = await storeEncryptedPrivateKey(account.privateKey);
             res.json({
                 address: account.address,
                 publicKey: publicKey,
                 privateKey: account.privateKey
             });
-            // console.log(account.privateKey);
-            // console.log("PRIVATE KEY :", getEncryptedPrivateKey());
-        // res.send(walletData);
     }
     else {
         res.status(400).json({ error: 'Invalid action' });
@@ -62,7 +63,7 @@ app.listen(PORT, () => {
 });
 
 // Store encrypted private key in a file
-async function storeEncryptedPrivateKey(encryptedPrivateKey) {
+async function storeEncryptedPrivateKey(userId, encryptedPrivateKey) {
     const key = crypto.scryptSync(passPhrase, 'salt', 32); // Derive key from passphrase (replace 'salt' with a unique value)
     const iv = crypto.randomBytes(16); // Generate IV (Initialization Vector)
     
@@ -70,19 +71,30 @@ async function storeEncryptedPrivateKey(encryptedPrivateKey) {
     let encryptedData = cipher.update(encryptedPrivateKey, 'utf8', 'hex');
     encryptedData += cipher.final('hex');
     
-    fs.writeFileSync(filePath, `${iv.toString('hex')}:${encryptedData}`);
-    return { encryptedPrivateKey: encryptedPrivateKey };
+    // fs.writeFileSync(filePath, `${iv.toString('hex')}:${encryptedData}`);
+    await Wallet.create({
+        userId: userId,
+        encryptedPrivateKey: encryptedData,
+        iv: iv.toString('hex')
+    });
+
 }
 
 // Retrieve encrypted private key from file
-async function getEncryptedPrivateKey() {
-    if (fs.existsSync(filePath)) {
-        const data = fs.readFileSync(filePath, 'utf8');
-        const parts = data.split(':');
-        const iv = Buffer.from(parts[0], 'hex');
-        const encryptedData = parts[1];
+async function getEncryptedPrivateKey(userId) {
+    const wallet = await Wallet.findOne({where: {userId: userId}});
+    // if (fs.existsSync(filePath)) {
+    if (wallet)
+    {
+        // const data = fs.readFileSync(filePath, 'utf8');
+        // const parts = data.split(':');
+        // const iv = Buffer.from(parts[0], 'hex');
+        // const encryptedData = parts[1];
 
+        const iv = Buffer.from(wallet.iv, 'hex');
         const key = crypto.scryptSync(passPhrase, 'salt', 32); // Derive key from passphrase (replace 'salt' with a unique value)
+        const encryptedData = wallet.encryptedPrivateKey;
+
         const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
         let decryptedPrivateKey = decipher.update(encryptedData, 'hex', 'utf8');
         decryptedPrivateKey += decipher.final('utf8');
@@ -91,3 +103,5 @@ async function getEncryptedPrivateKey() {
     }
     return null;
 }
+
+// WHEN I ADD LINE 38 THE BROWSER RELOADS ......................
